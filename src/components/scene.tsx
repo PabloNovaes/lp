b402ea79cf38d76aaa2865f24dad5cede7f76d12
◇օԹɑҍӀօժҽѵ◇ <133129982+PabloNovaes@users.noreply.github.com>
2025-05-07 16:54:07 -0300

    Delete src/components/scene.tsx

--- src/components/scene.tsx
+++
@@ -1,9 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export function Scene() {
-    return (
-        <div className='absolute w-full z-0 h-screen'>
-            <Spline scene="https://prod.spline.design/CFxiFoTdvhVu09aT/scene.splinecode" />
-        </div>
-    );
-}
