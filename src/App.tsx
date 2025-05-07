22f233bace948823cf28a130523202920bcee5a5
Pablo Novaes <ppablo.0028922@gmail.com>
2025-05-07 16:34:11 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Logo from "@/assets/logo.svg"
 import { MenuIcon } from "lucide-react"
+import { AnimatePresence, motion } from "motion/react"
 import { TextAnimate } from './components/magicui/text-animate'
-function App() {
 
+
+function App() {
   return (
     <>
       <div className="w-full h-full fixed bg-[url('https://framerusercontent.com/images/rR6HYXBrMmX4cRpXfXUOvpvpB0.png')] bg-repeat bg-[length:128px] rounded-none opacity-[0.075] absolute"></div>
@@ -16,15 +18,21 @@ function App() {
           </button>
         </header>
         <div className='flex flex-col items-center gap-10 mx-auto text-white relative sm:px-20 h-full pt-20'>
-          <div className="size-20 grid place-content-center relative">
-            <img src={Logo} className="w-full object-cover relative z-10" />
-            <img src={Logo} className="w-full object-cover blur-lg scale-150 absolute opacity-50 z-0 dark:opacity-30" />
-          </div>
+          <AnimatePresence>
+            <motion.div
+              initial={{ filter: "blur(10px)", y: 100, scale: .7, opacity: 0 }}
+              animate={{ filter: "blur(0)", y: 0, scale: 1, opacity: 1 }}
+              transition={{ delay: .2, duration: .4 }}
+              className="size-20 grid place-content-center relative">
+              <img src={Logo} className="w-full object-cover relative z-10" />
+              <img src={Logo} className="w-full object-cover blur-lg scale-150 absolute opacity-50 z-0 dark:opacity-30" />
+            </motion.div>
+          </AnimatePresence>
           <div className="grid gap-2">
-            <TextAnimate className='text-center font-medium text-[34px] leading-[32px] sm:text-6xl sm:leading-[56px] tracking-[-1px] max-w-[680px]' animation="blurInUp" by="character" once>
+            <TextAnimate delay={.4} className='text-center font-medium text-[34px] leading-[32px] sm:text-5xl sm:leading-[44px] tracking-[-1px] max-w-[680px]' animation="blurInUp" by="character" once>
               Assistenza Tecnica, Riparazioni e Soluzioni Digitali a Milano
             </TextAnimate>
-            <TextAnimate className='text-center text-zinc-300 text-md  sm:text-6xl sm:leading-[56px] tracking-[-1px] max-w-[680px]' animation="blurInUp" by="character" once>
+            <TextAnimate delay={.6} className='text-center text-zinc-300 text-md  sm:text-xl sm:leading-[56px] tracking-[-1px] max-w-[680px]' animation="blurInUp" by="character" once>
               Siamo al tuo fianco per ogni esigenza informatica, a casa o in azienda.
             </TextAnimate>
           </div>
